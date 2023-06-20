98d234a290fb25535419d1305079c26b311d340f
praveen-raj-143 <praveenrajgreath@gmail.com>
2023-06-20 11:31:49 +0530

    modified

--- Database/connection.js
+++ Database/connection.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery",true);
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/user")
+const connection = mongoose.connect("mongodb://0.0.0.0:27017/user")
 console.log("connected to database")
 module.exports= connection;
